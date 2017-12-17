a0060ea64b1f33e7acddd9b157bcfcaa2baad23b
Aris Nikolaou <aris.nikolaou@outlook.com>
2017-12-17 21:59:52 +0000

    Removed paket dependencies Added cake build  scripts Refactored typescanner Unit test stubs

---
+++ tests/FutureState.Common.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Example.xUnit.Tests
+{
+    public class UnitTest1
+    {
+        [Trait("Category","Sample")]
+        [Theory]
+        [InlineData(1, false)]
+        [InlineData(2, true)]
+        [InlineData(-1, false)]
+        [InlineData(0, false)]
+        public void Add(int number,bool expectedresult)
+        {
+            if(expectedresult)
+                2.Should().Be(number);
+            else
+                2.Should().NotBe(number);
+        }
+
+        [Fact]
+        public void Add2()
+        {
+            Assert.True(true);
+        }
+    }
+}
