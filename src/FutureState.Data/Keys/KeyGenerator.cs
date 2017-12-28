d48e0906ce52bfb9d972efd06997afb2bf8b30b8
Aris Nikolaou <aris.nikolaou@outlook.com>
2017-12-28 20:29:46 +0000

    Provide default constructor for KeyProvider Fixup in memory data access module Added unit tests

--- src/FutureState.Data/Keys/KeyGenerator.cs
+++ src/FutureState.Data/Keys/KeyGenerator.cs
@@ -9,6 +9,9 @@ namespace FutureState.Data
     {
         private readonly Func<TKey> _getGet;
 
+        /// <summary>
+        ///     Creates a new instance that uses a given function to generate new keys for a given entity type.
+        /// </summary>
         public KeyGenerator(Func<TKey> getGet)
         {
             Guard.ArgumentNotNull(getGet, nameof(getGet));
@@ -16,11 +19,22 @@ namespace FutureState.Data
             _getGet = getGet;
         }
 
+        /// <summary>
+        ///     Creates a new instance that generates default keys.
+        /// </summary>
+        public KeyGenerator() : this(() => default(TKey))
+        {
+            
+        }
+
+        /// <summary>
+        ///     Gets the entity type keys should be generated for.
+        /// </summary>
         public Type EntityType { get; } = typeof(TEntity);
 
-        public TKey GetNew()
-        {
-            return _getGet();
-        }
+        /// <summary>
+        ///     Gets a new key for a given entity.
+        /// </summary>
+        public TKey GetNew() => _getGet();
     }
 }
---
+++ tests/FutureState.Autofac.Tests/WiresUpServicesUsingInMemoryDataStoresStory.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using FutureState.Autofac.Modules;
+using FutureState.Data;
+using FutureState.Data.Providers;
+using FutureState.Reflection;
+using TestStack.BDDfy;
+using TestStack.BDDfy.Xunit;
+using Xunit;
+
+namespace FutureState.Autofac.Tests
+{
+    [Story]
+    public class WiresUpServicesUsingInMemoryDataStoresStory : IDisposable
+    {
+        private IContainer _container;
+        private IEnumerable<Contact> _items;
+        private IEnumerable<Address> _itemsFromGuidKeyedEntity;
+
+
+        protected void GivenAnInMemoryDrivenApp()
+        {
+            var cb = new ContainerBuilder();
+
+            cb.RegisterModule(new GenericDataServiceModule());
+            cb.RegisterModule(new InMemoryDataAccessModule());
+            cb.RegisterAll(new AppTypeScanner(Environment.CurrentDirectory, "FutureState.Autofac"));
+
+            _container = cb.Build();
+        }
+
+        protected void WhenAddingAnEntityToTheApp()
+        {
+            var entity = new Contact
+            {
+                Id = 1,
+                Name = "Name"
+            };
+
+            var provider = _container.Resolve<ProviderLinq<Contact, int>>();
+            provider.Add(entity);
+        }
+
+        protected void AndWhenQueryingPopulatedEntities()
+        {
+            var provider = _container.Resolve<ProviderLinq<Contact, int>>();
+            _items = provider.GetAll();
+        }
+
+        protected void AndWhenResolvingEntitiesFromUnPopulatedEntities()
+        {
+            var provider = _container.Resolve<ProviderLinq<Address, Guid>>();
+            _itemsFromGuidKeyedEntity = provider.GetAll();
+        }
+
+        protected void ThenShouldBeAbleToQueryItemsFromProviders()
+        {
+            Assert.Single(_items, m => m.Name == "Name");
+            Assert.Empty(_itemsFromGuidKeyedEntity);
+        }
+
+        [BddfyFact]
+        public void WiresUpServicesUsingInMemoryDataStores()
+        {
+            this.BDDfy();
+        }
+
+        public void Dispose()
+        {
+            _container?.Dispose();
+        }
+    }
+}
