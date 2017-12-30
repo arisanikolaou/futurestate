16e862f3398a6df20f73a64451c17bba38ac6d4d
Aris Nikolaou <aresan@hotmail.com>
2017-12-30 16:21:14 +0000

    Stubs for flow library

---
+++ src/FutureState.Flow/ErrorEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FutureState.Flow
+{
+    public class ErrorEvent
+    {
+        public string Type { get; set; }
+
+        public string Message { get; set; }
+    }
+}
