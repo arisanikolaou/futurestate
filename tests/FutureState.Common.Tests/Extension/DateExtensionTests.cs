d474544d8e9aff1d559a13560a37ea7f0ab8e0e9
Aris Nikolaou <aresan@hotmail.com>
2018-01-08 00:25:56 +0000

    Added missing unit date extensions test

---
+++ tests/FutureState.Common.Tests/Extension/DateExtensionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace FutureState.Common.Tests.Extension
+{
+    public class DateExtensionTests
+    {
+        [Theory]
+        [InlineData(1, 7, 30)]
+        [InlineData(1, 1, 24)]
+        public void DateTests(int startMonth, int endMonth, int expectedMonths)
+        {
+            DateTime start = new DateTime(2000, startMonth, 1);
+            DateTime end = new DateTime(2002, endMonth, 1);
+
+            Assert.Equal(expectedMonths, start.MonthDiff(end));
+        }
+    }
+}
