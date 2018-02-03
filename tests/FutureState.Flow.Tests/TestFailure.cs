96c6cd2dea4d19ab86edfb16914785ee4b57d112
Aris Nikolaou <aris.nikolaou@outlook.com>
2018-02-03 14:27:48 +0000

    Mark as fact

--- tests/FutureState.Flow.Tests/TestFailure.cs
+++ tests/FutureState.Flow.Tests/TestFailure.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 
 namespace FutureState.Flow.Tests
 {
     public class TestFailure
     {
+        [Fact]
         public void AlwaysFail()
         {
             Assert.False(true);
