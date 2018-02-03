32f6361b6ec13d287d3d8dd6cdb629305f6f0be8
Aris Nikolaou <aris.nikolaou@outlook.com>
2018-02-03 14:27:12 +0000

    Test precommit failure

--- src/FutureState.Flow/Properties/AssemblyInfo.cs
+++ src/FutureState.Flow/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.3.0")]
 [assembly: AssemblyFileVersion("0.3.0")]
 [assembly:
-    AssemblyInformationalVersion("0.1.0-unstable.147+Branch.develop.Sha.902eb94ce9a2505cab51ce6b5ec221b968e30d42")]
+    AssemblyInformationalVersion("0.1.0-unstable.157+Branch.develop.Sha.ac017426cc158a694bcb5676065b0716c934b507")]
